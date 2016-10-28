fbc82ca21e7042c85b7ff2a38302ba3aad3c5b60
Austin Blain <austin.a.blain@gmail.com>
2016-10-27 23:44:19 -0700

    make outputs random colors as well

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,37 @@
 //Business End Logic
+function deciding(yayaya) {
+  size = Math.floor(Math.random()*500);
+  if (size < 125) {
+    yayaya = "small";
+    return yayaya;
+  } else if (size >= 125 && size < 250 ) {
+    yayaya = "medium";
+    return yayaya;
+  } else if (size >= 250 && size < 375) {
+    yayaya = "big"
+    return yayaya;
+  } else if (size >= 375) {
+    yayaya = "biggest"
+    return yayaya;
+  }
+};
 
+function decidingColor(wawawa) {
+  color = Math.floor(Math.random()*500);
+  if (color < 125) {
+    yayaya = "blue";
+    return yayaya;
+  } else if (color >= 125 && color < 250 ) {
+    yayaya = "red";
+    return yayaya;
+  } else if (color >= 250 && color < 375) {
+    yayaya = "white"
+    return yayaya;
+  } else if (color >= 375) {
+    yayaya = "yellow"
+    return yayaya;
+  }
+}
 
 
 //Use Interface Log
@@ -11,57 +43,25 @@ $(document).ready(function() {
     var countBy = 1
     var size;
     var yayaya;
+    var wawawa;
     for (i = countBy; i <= countTo; i += countBy) {
       if (i % 3 === 0 && i % 5 === 0) {
         debugger;
-        size = Math.floor(Math.random()*500);
-        if (size < 125) {
-          yayaya = "small";
-        } else if (size >= 125 && size < 250 ) {
-          yayaya = "medium";
-        } else if (size >= 250 && size < 375) {
-          yayaya = "big"
-        } else if (size >= 375) {
-          yayaya = "biggest"
-        }
-        $("#output").append("<div>" + "<p>" + "pingpong" + "</p>" + "</div>").find('div:last').addClass(yayaya);
-        // $("#output div").addclass("4");
+        var cat = deciding(yayaya);
+        var dog = decidingColor(wawawa);
+        $("#output").append("<div>" + "<p>" + "pingpong" + "</p>" + "</div>").find('div:last').addClass(cat).addClass(dog);
       } else if (i % 3 === 0) {
-        size = Math.floor(Math.random()*500);
-        if (size < 125) {
-          yayaya = "small";
-        } else if (size >= 125 && size < 250 ) {
-          yayaya = "medium";
-        } else if (size >= 250 && size < 375) {
-          yayaya = "big"
-        } else if (size >= 375) {
-          yayaya = "biggest"
-        }
-        $("#output").append("<div>" + "<p>" + "ping" + "</p>" + "</div>").find('div:last').addClass(yayaya);
+        var cat = deciding(yayaya);
+        var dog = decidingColor(wawawa);
+        $("#output").append("<div>" + "<p>" + "ping" + "</p>" + "</div>").find('div:last').addClass(cat).addClass(dog);
       } else if (i % 5 === 0) {
-        size = Math.floor(Math.random()*500);
-        if (size < 125) {
-          yayaya = "small";
-        } else if (size >= 125 && size < 250 ) {
-          yayaya = "medium";
-        } else if (size >= 250 && size < 375) {
-          yayaya = "big"
-        } else if (size >= 375) {
-          yayaya = "biggest"
-        }
-        $("#output").append("<div>" + "<p>" + "pong" + "</p>" + "</div>").find('div:last').addClass(yayaya);
+        var cat = deciding(yayaya);
+        var dog = decidingColor(wawawa);
+        $("#output").append("<div>" + "<p>" + "pong" + "</p>" + "</div>").find('div:last').addClass(cat).addClass(dog);
       } else {
-        size = Math.floor(Math.random()*500);
-        if (size < 125) {
-          yayaya = "small";
-        } else if (size >= 125 && size < 250 ) {
-          yayaya = "medium";
-        } else if (size >= 250 && size < 375) {
-          yayaya = "big"
-        } else if (size >= 375) {
-          yayaya = "biggest"
-        }
-        $("#output").append("<div>" + "<p>" + i + "</p>" + "</div>").find('div:last').addClass(yayaya);
+        var cat = deciding(yayaya);
+        var dog = decidingColor(wawawa);
+        $("#output").append("<div>" + "<p>" + i + "</p>" + "</div>").find('div:last').addClass(cat).addClass(dog);
       }
     };
 
